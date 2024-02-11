aa61997651e229767d019dd38fc64a998e2339f0
mohansakireddy <mohansakireddy@gmail.com>
2024-02-11 14:30:29 +0530

    mohan commit

---
+++ src/main/java/com/example/demo/HospitalDbApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalDbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalDbApiApplication.class, args);
+	}
+
+}
